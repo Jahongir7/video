3d5f752514a09cc64f08bd0da7c9b19dc6693288
jahongir7 <jahongir95119@gmail.com>
2024-06-10 15:26:09 +0500

    fsdfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import "./index.css";
-import video1 from "/1.mp4";
+import video1 from "/11.mp4";
 import video2 from "/2.mp4";
 import video3 from "/3.mp4";
 
@@ -63,7 +63,7 @@ const App = () => {
           ></div>
         </div>
       ))} */}
-      <video src={video1} controls></video>
+      <video src={video1} controls loop></video>
     </div>
   );
 };
